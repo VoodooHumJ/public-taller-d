6c3785c9566b18acbfade41a5b7e6c2992dfbc9b
Humberto Chávez <tuemail@ejemplo.com>
2024-11-23 22:21:02 -0400

    index

--- contacto.js
+++ contacto.js
@@ -3,20 +3,21 @@ window.onload = function() {
     const logo2 = document.getElementById('logo2');
 
     setTimeout(() => {
-        menu.classList.add('show-menu'); // Agregar clase para mostrar el menú
-    }, 500); // 2000 ms = 2 segundos
+        menu.classList.add('show-menu'); 
+    }, 0);  
 
-    // Esperar 4 segundos (2 segundos del hero + 2 segundos del menú) antes de mostrar el logo
+     
     setTimeout(() => {
-        logo2.classList.add('show-logo'); // Agregar clase para mostrar el logo
-    }, 1000); // 4000 ms = 4 segundos
+        logo2.classList.add('show-logo');  
+    }, 0);  
 
 };
 
 document.addEventListener("DOMContentLoaded", function() {
-    // Selecciona el footer
+    
     const footer = document.querySelector('.footer');
     
-    // Agrega la clase 'fade-in' al footer para activar la transición
+    
     footer.classList.add('fade-in');
 });
+
--- script.js
+++ script.js
@@ -9,10 +9,10 @@ window.onload = function() {
     // Esperar 2 segundos (la duración de la transición del hero) antes de mostrar el menú
     setTimeout(() => {
         menu.classList.add('show-menu'); // Agregar clase para mostrar el menú
-    }, 2000); // 2000 ms = 2 segundos
+    }, 1000); // 2000 ms = 2 segundos
 
     // Esperar 4 segundos (2 segundos del hero + 2 segundos del menú) antes de mostrar el logo
     setTimeout(() => {
         logo2.classList.add('show-logo'); // Agregar clase para mostrar el logo
-    }, 4000); // 4000 ms = 4 segundos
+    }, 2000); // 4000 ms = 4 segundos
 };
