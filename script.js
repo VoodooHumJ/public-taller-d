d5da508ddd923524371cf22e62b7f31762aba47a
Humberto Chávez <tuemail@ejemplo.com>
2024-11-23 23:46:21 -0400

    mejor

--- script.js
+++ script.js
@@ -3,16 +3,16 @@ window.onload = function() {
     const menu = document.querySelector('.menu');
     const logo2 = document.getElementById('logo2');
 
-    // Mostrar el hero
+     
     hero.classList.add('show-hero');
 
-    // Esperar 2 segundos (la duración de la transición del hero) antes de mostrar el menú
+     
     setTimeout(() => {
-        menu.classList.add('show-menu'); // Agregar clase para mostrar el menú
-    }, 1000); // 2000 ms = 2 segundos
+        menu.classList.add('show-menu');  
+    }, 1000);  
 
-    // Esperar 4 segundos (2 segundos del hero + 2 segundos del menú) antes de mostrar el logo
+     
     setTimeout(() => {
-        logo2.classList.add('show-logo'); // Agregar clase para mostrar el logo
-    }, 2000); // 4000 ms = 4 segundos
+        logo2.classList.add('show-logo');  
+    }, 2000);  
 };
